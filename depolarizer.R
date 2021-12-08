34aabda1b1683edc9b50610ce51c328c412f7a2f
Iñaki Úcar <iucar@fedoraproject.org>
2021-12-08 20:36:45 +0100

    some fixes

--- depolarizer.R
+++ depolarizer.R
@@ -4,7 +4,7 @@ depolarizer <- function(img_i, res_o=1000, out.color=c(0, 0, 0)) {
   stopifnot(imager::width(img_i) == imager::height(img_i))
 
   dim_i <- imager::width(img_i)
-  res_i <- floor(sqrt(2 * (dim_i/2)^2))
+  res_i <- sqrt(2) * dim_i/2
 
   pix_o <- expand.grid(x=seq_len(res_o), y=rev(seq_len(res_o)))
   r <- res_i * exp(2*pi*(pix_o$y / res_o - 1))
@@ -13,17 +13,17 @@ depolarizer <- function(img_i, res_o=1000, out.color=c(0, 0, 0)) {
   cha_o <- imager::interp(img_i, pix_i)
 
   if (length(out.color) == 3) {
-    outside <- rowSums(sapply(pix_i, findInterval, c(1, dim_i)) != 1) > 0
+    outside <- rowSums(sapply(pix_i, findInterval, c(1, dim_i), TRUE) != 1) > 0
     for (i in seq_along(cha_o))
       cha_o[[i]][outside] <- out.color[i]
   }
 
   img_o <- imager::imfill(res_o, res_o, val=c(0, 0, 0))
-  for (i in seq_along(cha_o)) {
-    dim(cha_o[[i]]) <- dim(imager::channel(img_o, i))
-    imager::channel(img_o, i) <- cha_o[[i]]
-  }
+  for (i in seq_along(cha_o))
+    img_o[,,,i] <- cha_o[[i]]
   img_o
 }
 
-plot(depolarizer("circles.jpg"))
+if (sys.nframe() == 0) {
+  plot(depolarizer("circles.jpg"))
+}
--- depolarizer.py
+++ depolarizer.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import cv2 as cv
 import numpy as np
 
@@ -8,20 +7,22 @@ def depolarizer(img_i, res_o=1000):
     assert(img_i.shape[0] == img_i.shape[1])
   
     dim_i = img_i.shape[0]
-    res_i = np.floor(np.sqrt(2 * (dim_i/2)**2))
+    res_i = np.sqrt(2) * dim_i/2
 
     map_x = np.zeros((res_o, res_o), dtype=np.float32)
     map_y = np.zeros((res_o, res_o), dtype=np.float32)
 
-    for i in range(map_x.shape[0]):
-        for j in range(map_x.shape[1]):
-            r = res_i * np.exp(2*np.pi * (i / map_x.shape[0] - 1))
-            angle = 2*np.pi * j / map_x.shape[1]
-            map_x[map_x.shape[0] - 1 - i, j] = r * np.cos(angle) + dim_i/2
-            map_y[map_y.shape[0] - 1 - i, j] = r * np.sin(angle) + dim_i/2
+    for i in range(res_o):
+        for j in range(res_o):
+            r = res_i * np.exp(2*np.pi * (i / res_o - 1))
+            angle = 2*np.pi * j / res_o
+            map_x[res_o - 1 - i, j] = r * np.cos(angle) + dim_i/2
+            map_y[res_o - 1 - i, j] = r * np.sin(angle) + dim_i/2
 
     return cv.remap(img_i, map_x, map_y, cv.INTER_LINEAR)
 
-dst = depolarizer("circles.jpg")
-plt.imshow(cv.cvtColor(dst, cv.COLOR_BGR2RGB))
-plt.show()
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    dst = depolarizer("circles.jpg")
+    plt.imshow(cv.cvtColor(dst, cv.COLOR_BGR2RGB))
+    plt.show()
